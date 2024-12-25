53acce38ffffa4526f5868755ac2a3a0f60f5a6d
PavanDNaik <pawannaik396@gmail.com>
2024-12-25 12:18:30 +0530

    mobile view color

--- src/components/mobile/MobileView.jsx
+++ src/components/mobile/MobileView.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function MobileView() {
   return (
-    <div className="flex items-center justify-center h-screen bg-vs-background p-2 ">
-      <p className="text-2xl font-semibold text-text-white bg-vs-status p-4 rounded-lg shadow-lg">
+    <div class="flex items-center justify-center h-screen bg-vs-sid">
+      <p class="text-2xl font-semibold text-text-white bg-vs-nav p-6 rounded-lg shadow-xl">
         For the best experience, please use a laptop to view this website.
       </p>
     </div>
